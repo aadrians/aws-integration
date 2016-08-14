fcfa47864565766c293783cea187337486b9098a
arya <arya.adriansyah@gmail.com>
2016-08-14 22:24:56 +0200

    add 2 tests

--- aws-integration/src/main/java/lab/jenkins/bankaccount/aws_integration/AppTest2.java
+++ aws-integration/src/main/java/lab/jenkins/bankaccount/aws_integration/AppTest2.java
@@ -6,12 +6,12 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest2 
 {
 
     @Test
     public void testDebitWithSufficientFund(){
-    	System.out.println("Test is executed by JaCoCo");
+    	System.out.println("Test 2 is executed by JaCoCo");
     	BankAccount bankAccount = new BankAccount(10);
     	double amount = bankAccount.debit(5);
     	Assert.assertEquals(amount, 5.0, 0);
