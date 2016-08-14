cb212ec8e89f02ca8cac0d88f051bac55933ef9e
arya <arya.adriansyah@gmail.com>
2016-08-14 09:52:51 +0200

    Initial checkin

---
+++ aws-integration/src/main/java/lab/jenkins/bankaccount/aws_integration/App.java
@@ -0,0 +1,13 @@
+package lab.jenkins.bankaccount.aws_integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
